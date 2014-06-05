17c103d04a815504dec0c056e34bdbf99b0d9448
Hackbright Student <info@hackbrightacademy.com>
2014-06-04 17:55:22 -0700

    Fixed the array and got all functions working

--- calculator.py
+++ calculator.py
@@ -14,35 +14,66 @@ def main():
 	# open the file from arithmetic.py
     #my_file = open("arithmetic.py")
     # get input from user
-    math_input = raw_input("Please input a prefix notation")
+    #rray = []
+    math_input = raw_input("> ")
     array = []
-    array.append(math_input)
     array = math_input.split(" ")
     print array
 
     character = str(array[0])
     num1 = int(array[1])
-    num2 = int(array[2])
+    if len(array) == 3:
+        num2 = int(array[2])
+#    elif len(array) == 1:
+#        print "Please enter at least one character and one number."
+#    else:
+#        print "Please only use one character and two numbers."
+
+
+
+#    for character in array:
+
+    #pass_stmt = str(array[0])
+
+    if character == "+" or "-" or "*" or "/" or "square" or "cube" or "pow" or "mod":
+        if character == "+":
+            print arithmetic.add(num1,num2)
+            #break
+        elif character == "-":
+            print arithmetic.subtract(num1,num2)
+            #break
+        elif character == "*":
+            print arithmetic.multiply(num1,num2)
+            #break
+        elif character == "/":
+            print arithmetic.divide(num1,num2)
+            #break
+        elif character == "square":
+            print arithmetic.square(num1)
+            #break
+        elif character == "cube":
+            print arithmetic.cube(num1)
+            #break
+        elif character == "pow":
+            print arithmetic.power(num1,num2)
+            #break
+        elif character == "mod":
+            print arithmetic.mod(num1,num2)
+            #break
+        else:
+            print "Sorry, I do not understand. Please give me a mathematical character for calculation."
+
+
     # continue with a while loop for additional numbers
+    
+    #if pass_stmt == "+" or "-" or "*" or "/" or "square" or "cube" or "pow" or "mod":
+    #    return math_functions()
+    #else:
+    #    print "Sorry, I do not understand. Please try again."
+
+    #condition = True
 
     # start with calling the add function
-    if character == "+":
-        print arithmetic.add(num1,num2)
-    elif character == "-":
-        print arithmetic.subtract(num1,num2)
-    elif character == "*":
-        print arithmetic.multiply(num1,num2)
-    elif character == "/":
-        print arithmetic.divide(num1,num2)
-    elif character == "square":
-        print arithmetic.square(num1)
-    elif character == "cube":
-        print arithmetic.cube(num1)
-    elif character == "pow":
-        print arithmetic.power(num1,num2)
-    elif character == "mod":
-        print arithmetic.mod(num1,num2)
-
 
 
 
